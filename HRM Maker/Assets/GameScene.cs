b4e31e6aac002d1749d05d007f79538b6ba2fad4
Luzi82 <luzi82@gmail.com>
2017-05-30 23:22:46 +0800

    input output layout

---
+++ HRM Maker/Assets/GameScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour {
+
+	public Camera mCamera;
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Debug.Log("GameScene.Start");
+		UnityEngine.Debug.Log("camera.aspect.ToString: "+mCamera.aspect.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		UnityEngine.Debug.Log("GameScene.Update");
+	}
+}
