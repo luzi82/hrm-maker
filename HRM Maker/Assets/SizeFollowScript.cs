f2c45dfc9a21584b40463464dc55ef4846869bbf
Luzi82 <luzi82@gmail.com>
2017-06-07 22:41:09 +0800

    Unity upgrade to 2017.1.0b8 (and extra?)

---
+++ HRM Maker/Assets/SizeFollowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SizeFollowScript : MonoBehaviour {
+
+	public GameObject parent;
+
+	private Vector2 oldSize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(parent==null)return;
+		RectTransform prt = (RectTransform)parent.GetComponent(typeof(RectTransform));
+		Vector2 nowSize = prt.rect.size;
+		if(oldSize!=nowSize){
+			RectTransform rt = (RectTransform)GetComponent(typeof(RectTransform));
+			rt.sizeDelta = nowSize;
+			oldSize=nowSize;
+		}
+	}
+}
